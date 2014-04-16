f0a5578881d09dd5e84eb380ac46177d41d3ebcf
Sébastien Blin <amarok@enconn.fr>
2014-04-16 09:15:37 +0200

    algo gen, some bugs

--- copyImage.py
+++ copyImage.py
@@ -1,8 +1,10 @@
+
 #-----------------------------------------------------------------
-#| 7 bits posx | 7 bits posy | 8 red | 8 green | 8 blue | 5 size |
+#| 7 bits posx | 7 bits posy | 8 red | 8 green | 8 blue | 3 size |
 #-----------------------------------------------------------------
 import random
-import cv2
+import cv, cv2
+import numpy as np
 
 def toBin(number, nCarac):
     return str(bin(number)[2:len(bin(number))]).zfill(nCarac)
@@ -29,7 +31,7 @@ def mutateGene(gene, n):
 
 def createGenome():
     population = []
-    for i in range(0,400):
+    for i in range(0,2000):
         population.append([generateGene(), 0])
     return population
 
@@ -49,22 +51,27 @@ def crossGenome(dadGen, motherGen):
 
 def translateGene(gene):
     posx = int(gene[:7], 2)
-    posy = int(gene[8:14], 2)
+    posy = int(gene[7:14], 2)
     red = int(gene[14:22], 2)
-    green = int(gene[23:30], 2)
-    blue = int(gene[31:39], 2)
-    size = int(gene[40:45], 2)
-    if size > 100:
-        size = 10
+    green = int(gene[22:30], 2)
+    blue = int(gene[30:38], 2)
+    size = int(gene[38:41], 2)
     if posx > 100:
-        posx = 10
+        posx = 100
     if posy > 100:
-        posy = 10
+        posy = 100
     return [posx, posy, red, green, blue, size]
 
-#TODO : test
 def drawRect(image, posx, posy, red, green, blue, size):
-    cv2.rectangle(image, [posx, posy], [posx + len(image)*size/100, posy + len(image[0])*size/100], [red, green, blue])
+    image[posx*len(image)/100:posx*len(image)/100 + len(image)*size/100, posy*len(image)/100:posy*len(image)/100 + len(image)*size/100] = [red, green, blue]
+    return image
+
+def createImageFromGenome(genome):
+    image = np.zeros((800, 800, 3), np.uint8)
+    cv2.imwrite('test.png', image)
+    for gene, score in genome:
+        translation = translateGene(gene)
+        image = drawRect(image, translation[0], translation[1], translation[2], translation[3], translation[4], translation[5])
     return image
 
 #TODO Prendre la distance px, ajouter le tout, diviser par le nombre de px. 
@@ -73,11 +80,8 @@ def evaluateImage(image, base):
     #...
     return 0
 
-dadGen = createGenome()
-print(translateGene(dadGen[0][0]))
-#motherGen = createGenome()
-#print(dadGen == motherGen)
-#sonGen = crossGenome(dadGen, motherGen)
-#print(dadGen == sonGen)
-#print(sonGen == motherGen)
-#Pendant la lecture du gene, voir si il est correct.
+population = []
+for i in range(0,100):
+    population.append(createGenome())
+for i in range(0, len(population)):
+    cv2.imwrite('test'+str(i)+'.png', createImageFromGenome(population[i]))
